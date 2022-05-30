a062b8a27c9180ed14ff3c14865e7612934c094f
galortega <galfeort@espol.edu.ec>
2022-05-29 23:00:53 -0500

    Final

--- unit1/mycat/main.c
+++ unit1/mycat/main.c
@@ -1,30 +1,45 @@
+/*
+ * File:   mycat.c
+ * Author: galortega
+ *
+ * Created on May 28, 2022, 18:17 PM
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#define BUFFER_SIZE 1024
 
 int myCatfd(int ifd, int ofd);
-int myCatfp(FILE *ifp, FILE *ofp);
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
+    if (argc == 1) // stdin to stdout
     {
         if (myCatfd(0, 1) != 0)
+        {
             fprintf(stderr, "failed to copy standard input\n");
+            perror(NULL); // print error message
+        }
     }
-    else
+    else // if argument is given
     {
         for (int i = 1; i < argc; i++)
         {
             int fd = open(argv[i], O_RDONLY); // had to include fcntl.h to get O_RDONLY
             if (fd < 0)
+            {
                 fprintf(stderr, "failed to open %s for reading\n", argv[i]);
+                perror(NULL);
+            }
             else
             {
                 if (myCatfd(fd, 1) != 0)
-                    //fprintf(stderr, "failed to copy %d to standard output\n", argv[i]);
+                {
+                    fprintf(stderr, "failed to copy %s to standard output\n", argv[i]);
+                    perror(NULL);
+                }
                 close(fd); // had to include unistd.h to get close()
             }
         }
@@ -33,14 +48,19 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+// myCatfd: copies the contents of the file descriptor ifd to the file descriptor ofd
 int myCatfd(int ifd, int ofd)
 {
-    char buffer[4096];
+    char buffer[BUFFER_SIZE];
     ssize_t nbytes;
-    while ((nbytes = read(ifd, buffer, sizeof(buffer))) > 0)
+    while ((nbytes = read(ifd, buffer, sizeof(buffer))) > 0) // had to include unistd.h to get read()
     {
-        if (write(ofd, buffer, nbytes) != nbytes)
-            return -1;
+        if (write(ofd, buffer, nbytes) != nbytes) // had to include unistd.h to get write()
+        {
+            perror(NULL);
+            fprintf(stderr, "failed to write to standard output\n");
+            return -1; // had to include stdlib.h to get EXIT_FAILURE
+        }
     }
     return (nbytes < 0) ? -1 : 0;
 }
